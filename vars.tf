17e4202384f23bd5f0cad22bbd2f51f531268701
EC2 Default User <ec2-user@ip-10-0-1-138.ec2.internal>
2021-07-28 18:24:59 +0000

    updated variable

--- vars.tf
+++ vars.tf
@@ -3,7 +3,7 @@ variable "aws_secret_key" {}
 variable "aws_region" {}
 variable "aws_key_name" {}
 variable "aws_key_fingerprint" {}
-variable "aws_certificate_arn" {}
+#variable "aws_certificate_arn" {}
 variable "allowed_ip_network" {}
 variable "eks_role_arn" {}
 variable "eks_node_max" {}
